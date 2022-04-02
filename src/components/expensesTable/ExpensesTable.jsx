43211f2b09a4e739912b1a00d60a288dca57a642
GvidasLu <luspinis.gvidas@gmail.com>
2022-04-02 23:10:00 +0300

    Commented const's

--- src/components/expensesTable/ExpensesTable.jsx
+++ src/components/expensesTable/ExpensesTable.jsx
@@ -1,37 +1,40 @@
-import {Table} from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import Expense from "../expense/Expense";
 import React from "react";
 
+const ExpensesTable = (props) => {
+  // const {id}=useParams();
+  // const navigate = useNavigate();
 
-const ExpensesTable = (props)=>{
-    return(
-        <Table striped bordered hover>
-            <thead>
-            <tr>
-                <th>Data</th>
-                <th>Išlaidų tipas</th>
-                <th>Aprašymas</th>
-                <th>Suma</th>
-                <th>Redagavimas</th>
-                <th>Šalinimas</th>
-            </tr>
-            </thead>
-            <tbody>
-            {props.data.map((expense, i) => {
-                console.log(i)
-                return (<Expense
-                    key={i}
-                    id = {expense.id}
-                    date={expense.date}
-                    type={expense.type}
-                    description={expense.description}
-                    amount={expense.amount}
-                />)
-            })
-            }
-            </tbody>
-        </Table>
-    )
-}
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Išlaidų tipas</th>
+          <th>Aprašymas</th>
+          <th>Suma</th>
+          <th>Redagavimas</th>
+          <th>Šalinimas</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((expense, i) => {
+          console.log(i);
+          return (
+            <Expense
+              key={i}
+              id={expense.id}
+              date={expense.date}
+              type={expense.type}
+              description={expense.description}
+              amount={expense.amount}
+            />
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
 
-export default ExpensesTable
+export default ExpensesTable;
